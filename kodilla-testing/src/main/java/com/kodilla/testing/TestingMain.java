19fb451286807de31b28405bf4dc13f4ea95a86d
antykom <36765862+antykom@users.noreply.github.com>
2022-05-14 14:26:02 +0200

    6.2 Added Calculator testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -10,18 +10,18 @@ public class TestingMain {
 
         String result = simpleUser.getUsername();
         if (result.equals("theForumUser")) {
-            System.out.println("Test OK");
+            System.out.println("Test USERNAME ok");
         } else {
             System.out.println("Error!");
         }
 
-        Calculator calc = new Calculator(10, 31);
+        Calculator calc = new Calculator();
 
-        int addResult = calc.add();
-        int subtractResult = calc.subtract();
+        int addResult = calc.add(10, 31);
+        int subtractResult = calc.subtract(10, 31);
 
         if(addResult == 41 && subtractResult == -21){
-            System.out.println("Test OK");
+            System.out.println("Test CALCULATOR ok");
         } else {
             System.out.println("Error!");
         }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -4,16 +4,12 @@ public class Calculator {
     private int a;
     private int b;
 
-    public Calculator(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
 
-    public int add(){
+    public int add(int a, int b){
         return a + b;
     }
 
-    public int subtract(){
+    public int subtract(int a, int b){
         return a - b;
     }
 }
