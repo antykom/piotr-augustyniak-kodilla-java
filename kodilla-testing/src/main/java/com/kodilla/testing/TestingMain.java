50513c03c2f1968a80fd1c7c919e64e15f14d65f
antykom <36765862+antykom@users.noreply.github.com>
2022-05-14 13:57:27 +0200

    6.2 Added Calculator testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calc = new Calculator(10, 31);
+
+        int addResult = calc.add();
+        int subtractResult = calc.subtract();
+
+        if(addResult == 41){
+            System.out.println("Test ADD ok!!");
+        } else {
+            System.out.println("Test ADD fail!!");
+        }
+
+        if(subtractResult == -21){
+            System.out.println("Test SUBTRACT ok!!");
+        } else {
+            System.out.println("Test SUBTRACT fail!!");
+        }
     }
 }
