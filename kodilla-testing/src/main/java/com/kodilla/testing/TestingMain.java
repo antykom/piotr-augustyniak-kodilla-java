c1e9bc024a668e9cdc07ebb3e4163dab3e2d95ae
antykom <36765862+antykom@users.noreply.github.com>
2022-05-17 08:47:55 +0200

    6.2Added Calculator testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
-
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("Test USERNAME ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+
+        int addResult = calc.add(10, 31);
+        int subtractResult = calc.subtract(10, 31);
+
+        if(addResult == 41 && subtractResult == -21){
+            System.out.println("Test CALCULATOR ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
